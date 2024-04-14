9b028924ee6489a9f332145f93aac0026c27d2ba
David Tosheski <david.tosheski23@gmail.com>
2024-04-14 19:34:59 +0200

    Fixed errors in SILab1_1 and SILab1_2

--- SILab1_2.java
+++ SILab1_2.java
@@ -1,14 +1,11 @@
 import java.util.List;
 
-public class SILab1_2 {
-
-    private static String makeBigStringFrom (List<String> list, int minLength) {
-        StringBuilder sb = new StringBuilder();
-        for (String el : list) {
-            if (el.length() < minLength) {
-                sb.append(el);
-            }
+private static String makeBigStringFrom(List<String> list, int minLength) {
+    StringBuilder sb = new StringBuilder();
+    for (String el : list) {
+        if (el.length() >= minLength) { // smenet uslov
+            sb.append(el);
         }
-        return sb.toString();
     }
+    return sb.toString();
 }
